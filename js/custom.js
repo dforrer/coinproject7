2ca36c14eb3055e51ff78fb1e0fa83f145764972
Daniel <dforrer@gmail.com>
2016-06-08 21:40:00 +0200

    animated carousel height change

--- js/custom.js
+++ js/custom.js
@@ -33,6 +33,18 @@ function updateNav () {
 
 }
 
+function bsCarouselAnimHeight()
+{
+    $('.carousel').carousel({
+        interval: 5000
+    }).on('slide.bs.carousel', function (e)
+    {
+        var nextH = $(e.relatedTarget).height();
+        $(this).find('.active.item').parent().animate({ height: nextH }, 500);
+    });
+}
+
 window.onload = function() {
     updateNav();
+    bsCarouselAnimHeight();
 };

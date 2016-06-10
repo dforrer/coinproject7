8e0051570f6817e0c87787cd8377897028e38d88
Daniel <dforrer@gmail.com>
2016-06-10 15:07:05 +0200

    CSS adjustments + long titles with line-breaks + smooth scrolling

--- js/custom.js
+++ js/custom.js
@@ -47,4 +47,26 @@ function bsCarouselAnimHeight()
 window.onload = function() {
     updateNav();
     bsCarouselAnimHeight();
+
+    // Enable smooth scrolling globally
+    // handle links with @href started with '#' only
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $(id).offset().top;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos});
+    });
 };
